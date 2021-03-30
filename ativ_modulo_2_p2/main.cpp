2d1ceee96977245cf8339012d051439f942b6631
benjamim <bneto@computacao.cua.ufmt.br>
2021-03-30 19:51:17 -0300

    public, private, protected

--- ativ_modulo_2_p2/main.cpp
+++ ativ_modulo_2_p2/main.cpp
@@ -4,16 +4,41 @@
 
 using namespace std;
 
-class Empresa{
-  private:
-    string nome;
-    int codigoFuncional;
-    string escola;
-    string nivel;
-    
+class Funcionario{
+protected:
+	string nome;
+	int codigoFuncional;
+	double salario;
+public:
+	Funcionario(string nome, int codigoFuncional):nome(nome),codigoFuncional(codigoFuncional){};
 
+	void setNome(string nome){this->nome = nome;}
+	void setSalario(double salario){this->salario = salario;}
+	void setCodigoFuncional(int codigoFuncional){this->codigoFuncional = codigoFuncional;}
+
+	string getNome(){return this->nome;}
+	double getSalario(){return this->salario;}
+	int getCodigoFuncional(){return this->codigoFuncional;}
 
 };
+
+class FuncionarioBasico : public Funcionario{
+private:
+	string escola;
+public:
+	Funcionario(string escola):Funcionario(nome,codigoFuncional,salario),escola(escola){
+		this->nome = nome;
+		this->codigoFuncional = codigoFuncional;
+		this->salario = salario
+	}
+	void imprimeDados(){
+		cout<< this->escola<< this->nome<< this->codigoFuncional<< this->salario<<endl;
+	}
+}
+
+
+
+
 
 int main() {
   
