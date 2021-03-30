159bcc80e440417b27c98df6f954f814b063c3a7
benjamim <bneto@computacao.cua.ufmt.br>
2021-03-30 19:24:39 -0300

    trab 2

---
+++ ativ_modulo_2_p2/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Empresa{
+  private:
+    string nome;
+    int codigoFuncional;
+    string escola;
+    string nivel;
+    
+
+
+};
+
+int main() {
+  
+}
